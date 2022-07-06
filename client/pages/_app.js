e78f2445e564613546c98e85312dbe8a4d5a9d9b
Shankar4919 <itsme.shankar8@gmail.com>
2022-07-06 13:12:01 +0545

    :white_check_mark: ant menu and icon in navbar

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -2,9 +2,16 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "antd/dist/antd.css";
 import "../public/CSS/style.css";
 
+import "../components/TopNav";
+import TopNav from "../components/TopNav";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <TopNav />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
---
+++ client/pages/register.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Register = () => {
+  return (
+    <>
+      <h1 className="jumbotron text-center bg-primary">Register</h1>
+      <p className="text-center">This is the Register page</p>
+    </>
+  );
+};
+
+export default Register;
