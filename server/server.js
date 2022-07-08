9f28e54c218d5a6265a70afa13b2d9f9c0885916
Shankar4919 <itsme.shankar8@gmail.com>
2022-07-08 13:51:57 +0545

    :white_check_mark: db connection with mongoose done

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@
 import express, { application } from 'express';
 import cors from 'cors';
 
+import mongoose from 'mongoose'; // to connect to mongodb
+
 import { readdirSync } from 'fs';  // readdirSync is a function that reads a directory and returns an array of files
 
 // importing morgan package using import syntax gives an errors so use require syntax to import it
@@ -12,7 +14,20 @@ require('dotenv').config(); //help load environment variables
 //create express app
 const app = express();
 
-//create middleware
+//connect to mongodb
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    console.log('**DB CONNECTED**');
+}).catch(err => {
+    console.log("DB Connection Error: " + err);
+}
+);
+
+
+
+//apply middleware
 
 app.use(cors()); // enable cors
 app.use(express.json());  // parse incoming requests data to json
