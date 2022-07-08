1e86b20ae29dceecc48b8423f6bbfb3515a6408e
Shankar4919 <itsme.shankar8@gmail.com>
2022-07-08 11:27:27 +0545

    :tada: server ready to go

--- server/server.js
+++ server/server.js
@@ -1,1 +1,34 @@
-console.log("Let's Go! Server Started");
+
+import express, { application } from 'express';
+import cors from 'cors';
+
+// importing morgan package using import syntax gives an errors so use require syntax to import it
+const morgan = require('morgan');	
+
+require('dotenv').config(); //help load environment variables
+
+//create express app
+const app = express();
+
+//create middleware
+
+app.use(cors()); // enable cors
+app.use(express.json());  // parse incoming requests data to json
+app.use(morgan('dev'));  // log requests to console
+app.use((req, res, next) => {
+    console.log("I am middleware");
+    next();
+});
+
+//route
+app.get('/', (req, res) => {
+    res.send('You hit the end point');
+});
+
+//port
+const port = process.env.PORT || 5000;
+
+//start server
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+})
