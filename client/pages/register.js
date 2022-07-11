e350932af87b398ab79b80711250e4ca2e446b4d
Shankar4919 <itsme.shankar8@gmail.com>
2022-07-11 16:55:27 +0545

    :white_check_mark: managing api route in env local file

--- client/pages/register.js
+++ client/pages/register.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import axios from "axios";
 import { toast } from "react-toastify";
 import { Typography, Divider, Link } from "antd";
+import { SyncOutlined } from "@ant-design/icons";
 
 const { Title } = Typography;
 
@@ -10,26 +11,35 @@ const Register = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  // console.log(process.env.NEXT_PUBLIC_API);
 
   const handleSubmit = async (e) => {
     e.preventDefault(); // prevents page from refreshing on submit
     try {
-      const { data } = await axios.post(`http://localhost:8000/api/register`, {
-        name,
-        email,
-        password,
-      });
+      setLoading(true);
+      const { data } = await axios.post(
+        `${process.env.NEXT_PUBLIC_API}/register`,
+        {
+          name,
+          email,
+          password,
+        }
+      );
       // console.log("Register Data", data);
       toast.success("Register Successful. Please Login");
+      setLoading(false);
     } catch (err) {
       toast.error(err.response.data);
+      setLoading(false);
     }
   };
 
   return (
     <>
       <div className="container col-md-4 offset-md-4 p-4 mt-4">
-        <Title level={5}>Sign up and Starting Learning</Title>
+        <Title level={4}>Sign up and Starting Learning</Title>
         <form onSubmit={handleSubmit}>
           <input
             type="text"
@@ -40,7 +50,7 @@ const Register = () => {
             onChange={(e) => setName(e.target.value)}
             style={{
               height: "4rem",
-            
+              fontSize: "1.2rem",
               border: "1px solid black",
             }}
             required
@@ -54,7 +64,7 @@ const Register = () => {
             onChange={(e) => setEmail(e.target.value)}
             style={{
               height: "4rem",
-           
+              fontSize: "1.2rem",
               border: "1px solid black",
             }}
             required
@@ -68,7 +78,7 @@ const Register = () => {
             onChange={(e) => setPassword(e.target.value)}
             style={{
               height: "4rem",
-            
+              fontSize: "1.2rem",
               border: "1px solid black",
             }}
             required
@@ -76,26 +86,38 @@ const Register = () => {
           <div className="d-grid gap-2 mt-2">
             <button
               className="btn btn-block btn-primary"
+              type="sign up"
               style={{
                 height: "4rem",
-               
+                fontSize: "1.5rem",
+                fontWeight: "bold",
                 border: "1px solid black",
               }}
             >
-              Submit
+              {loading ? <SyncOutlined spin /> : "Sign Up"}
             </button>
           </div>
-          <Typography.Paragraph className="mt-2" style={{
-            textAlign: "center",
-          }}>
+          <Typography.Paragraph
+            className="mt-2"
+            style={{
+              textAlign: "center",
+            }}
+          >
             By signing up, you agree to our Terms of Use and Privacy Policy
           </Typography.Paragraph>
-          <Divider />
-          <Typography.Paragraph style={{
-            textAlign: "center",
-          }}>
-            Already have an account?<a href="/" style={{color:"red"}}> Login</a>
-          </Typography.Paragraph>
+          <Divider style={{ border: "0.5px solid #edf0f1" }} />
+          <Title
+            level={5}
+            style={{
+              textAlign: "center",
+            }}
+          >
+            Already have an account?
+            <a href="/" style={{ color: "red" }}>
+              {" "}
+              Login
+            </a>
+          </Title>
         </form>
       </div>
     </>
