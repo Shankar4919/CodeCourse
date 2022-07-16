16b604247db4551733e540d0e0be009ff3dd8127
Shankar4919 <itsme.shankar8@gmail.com>
2022-07-16 13:19:19 +0100

    :ok_hand:aceeditor

--- client/src/components/Practice/Practice.jsx
+++ client/src/components/Practice/Practice.jsx
@@ -4,6 +4,19 @@ import { Row, Col, Form, Button, Spinner } from "react-bootstrap";
 import { FaCaretSquareRight } from "react-icons/fa";
 
 import AceEditor from "react-ace";
+import "ace-builds/src-noconflict/mode-java";
+import "ace-builds/src-noconflict/mode-python";
+import "ace-builds/src-noconflict/mode-c_cpp";
+import "ace-builds/src-noconflict/mode-javascript";
+import "ace-builds/src-noconflict/mode-golang";
+import "ace-builds/src-noconflict/mode-ruby";
+import "ace-builds/src-noconflict/mode-php";
+import "ace-builds/src-noconflict/mode-csharp";
+import "ace-builds/src-noconflict/mode-typescript";
+import "ace-builds/src-noconflict/theme-github";
+import "ace-builds/src-noconflict/theme-monokai";
+import "ace-builds/src-noconflict/theme-tomorrow";
+import "ace-builds/src-noconflict/theme-twilight";
 import "ace-builds/src-noconflict/theme-xcode";
 import "ace-builds/src-noconflict/theme-terminal";
 import "ace-builds/src-noconflict/theme-textmate";
@@ -18,42 +31,7 @@ import "./Practice.css";
 export default function Practice() {
   const navigate = useNavigate();
 
-  const [loading, setLoading] = useState(false);
-
-  const [name, setName] = useState("");
   
-
-  useEffect(() => {
-    if (!localStorage.getItem("token")) {
-      navigate("/login");
-    } else {
-      fetch(`/api/user/verifyToken`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          token: localStorage.getItem("token"),
-        }),
-      })
-        .then((response) => response.json())
-        .then((data) => {
-          if (!data.user) {
-            localStorage.removeItem("token");
-            navigate("/");
-          } else {
-            setName(data.user.name);
-          }
-        });
-    }
-
-    setLoading(true);
-    setTimeout(() => {
-      setLoading(false);
-    }, 2500);
-  }, [navigate]);
-
-     
 
   return (
     <div style={{backgroundColor:"white"}}>
@@ -76,17 +54,40 @@ export default function Practice() {
                     value={language}
                     onChange={(e) => setLanguage(e.target.value)}
                   >
-                    
+                    <option value="c_cpp">C/C++</option>
+                    <option value="golang">Go</option>
+                    <option value="java">Java</option>
+                    <option value="javascript">JavaScript</option>
+                    <option value="python">Python</option>
+                    <option value="ruby">Ruby</option>
+                    <option value="php">PHP</option>
+                    <option value="csharp">C#</option>
+                    <option value="typescript">TypeScript</option>
                   </Form.Select>
                 </Col>
                 <Col>
                   <Form.Label>Select Theme</Form.Label>
-                  
-                    
+                  <Form.Select
+                    value={theme}
+                    onChange={(e) => setTheme(e.target.value)}
+                  >
+                    <option value="github">Github</option>
+                    <option value="monokai">Monokai</option>
+                    <option value="tomorrow">Tomorrow</option>
+                    <option value="twilight">Twilight</option>
+                    <option value="xcode">XCode</option>
+                    <option value="terminal">Terminal</option>
+                    <option value="textmate">Textmate</option>
+                  </Form.Select>
                 </Col>
 
                 <Col>
-                  
+                  <Button
+                    variant="outline-success"
+                    className="run-button"
+                    disabled={compiling}
+                    onClick={handleCodeCompile}
+                  >
                     <FaCaretSquareRight /> Run Code
                   </Button>
                 </Col>
@@ -94,7 +95,72 @@ export default function Practice() {
               </Row>
               <Row>
                 <Col>
-                  
+                  <AceEditor
+                    placeholder="Enter Code Here"
+                    mode={language}
+                    theme={theme}
+                    onChange={(newValue) => {
+                      setCode(newValue);
+                    }}
+                    fontSize={16}
+                    showPrintMargin={true}
+                    showGutter={true}
+                    highlightActiveLine={true}
+                    value={code}
+                    style={{ width: "50rem" }}
+                    setOptions={{
+                      enableBasicAutocompletion: true,
+                      enableLiveAutocompletion: true,
+                      enableSnippets: false,
+                      showLineNumbers: true,
+                      tabSize: 4,
+                    }}
+                  />
+                </Col>
+
+                <Col>
+                  <div style={{ marginTop: "-7vh", paddingLeft:"2.5vh" }}>
+                    <Row>
+                      <Form.Label>Enter Input</Form.Label>
+                      <div>
+                      <Form.Control
+                        className="input"
+                        as="textarea"
+                        placeholder="Guess the Outcome"
+                        rows="3"
+                        value={input}
+                        onChange={(e) => setInput(e.target.value)}
+                      />
+                      </div>
+                    </Row>
+                    <Row>
+                      <div>
+                        {compiling ? (
+                          <Spinner animation="border" variant="primary" />
+                        ) : null}
+                        {output !== "" && !error ? (
+                          <div className="output-window">
+                            <h3>Output</h3>
+                            <div className="output" style={{marginTop:"-1vh", borderRadius:"5px"}}>{output}</div>
+                            <div className="time-memory">
+                              <div className="time">
+                                <FaClock /> Time: {time} seconds
+                              </div>
+                              <div className="memory">
+                                <MdMemory /> Memory: {memory} KB
+                              </div>
+                            </div>
+                          </div>
+                        ) : null}
+                        {error ? (
+                          <div className="error-window">
+                            <h3>Error</h3>
+                            <div className="error">{errorMessage}</div>
+                          </div>
+                        ) : null}
+                      </div>
+                    </Row>
+                  </div>
                 </Col>
               </Row>
             </div>
