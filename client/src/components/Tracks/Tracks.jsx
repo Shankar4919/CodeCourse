3a6d2cae1b1306677fdb6a6a86643be0d729672e
Shankar4919 <itsme.shankar8@gmail.com>
2022-07-17 13:19:19 +0100

    :wrench: single course track

---
+++ client/src/components/Tracks/Tracks.jsx
@@ -0,0 +1,69 @@
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { Card, Button, Row, Col } from "react-bootstrap";
+import PreLoader from "../PreLoader/PreLoader";
+import UserNavBar from "../UserNavBar/UserNavBar";
+import Footer from "../Footer/Footer";
+import "./Tracks.css";
+
+export default function Tracks() {
+  const navigate = useNavigate();
+
+  const [loading, setLoading] = useState(false);
+  const [tracks, setTracks] = useState([]);
+
+  return (
+    <div>
+      {loading ? (
+        <PreLoader />
+      ) : (
+        <div>
+          {/* Navbar compnent */}
+          <UserNavBar />
+
+          <div className="tracks container">
+            {/* main heading */}
+            <div className="main-heading">
+              <h2>Avaliable Tracks</h2>
+            </div>
+
+            {/* tracks cards */}
+            <div className="tracks-card">
+              <Row>
+                {tracks.map((track) => (
+                <Col key={track._id}>
+                  <Card>
+                    <Card.Body>
+                      <Card.Img
+                        fluid="true"
+                        variant="top"
+                        src={track.image}
+                      />
+                      <Card.Title>{track.name}</Card.Title>
+                      <Card.Text>
+                        {track.description}
+                      </Card.Text>
+                      <Button
+                        variant="success"
+                        onClick={(e) => {
+                          e.preventDefault();
+                          navigate(`/track/${track._id}`);
+                        }}
+                      >
+                        Prepare
+                      </Button>
+                    </Card.Body>
+                  </Card>
+                </Col>
+                ))}
+              </Row>
+            </div>
+          </div>
+
+          {/* footer */}
+          <Footer />
+        </div>
+      )}
+    </div>
+  );
+}
